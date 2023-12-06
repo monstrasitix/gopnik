5ad399dd7c927f39fa69e3003672d7f10b229f03
monstrasitix <injectedinferno@gmail.com>
2023-12-06 23:09:30 +0000

    Stuck on TS

--- web/script/form.ts
+++ web/script/form.ts
@@ -1,1 +1,31 @@
-console.log("Hey there");
+type FieldValues = Record<string, string>;
+
+
+function getValues(form: HTMLFormElement): FieldValues {
+    const values: FieldValues = {};
+
+    for (const element of form.elements) {
+        if (element instanceof HTMLInputElement) {
+            values[element.name] = element.value;
+        }
+    }
+
+    return values;
+}
+
+
+function onChange(ev: Event) {
+
+}
+
+function onSubmit(ev: SubmitEvent) {
+    ev.preventDefault();
+
+
+    if (ev.target instanceof HTMLFormElement) {
+        console.log(getValues(ev.target));
+    }
+}
+
+document.addEventListener("submit", onSubmit);
+document.addEventListener("change", onChange);
